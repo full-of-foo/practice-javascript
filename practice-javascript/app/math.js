f50b9e6fe59b8aafc77061c5fe24ad80e352bdcf
Anthony <lightweightdevelopment@gmail.com>
2017-04-01 18:40:24 +0100

    Added a little more math

--- practice-javascript/app/math.js
+++ practice-javascript/app/math.js
@@ -14,4 +14,21 @@ export default () => {
     console.log('toExponential (2): ', a.toExponential(2), a);
     console.log('toExponential (10): ', a.toExponential(10), a);
 
+    console.log('toPrecision for fixing decimal regarless of decimal place (no arg): ', a.toPrecision(), a);
+    console.log('toPrecision (1): ', a.toPrecision(1), a);
+    console.log('toPrecision (2): ', a.toPrecision(2), a);
+    console.log('toPrecision (10): ', a.toPrecision(10), a);
+
+    const b = 24;
+    console.log('toString for displaying numbers', b.toString(), b);
+    console.log('toString(10) for base 10', b.toString(10), b);
+    console.log('toString(16) for hex', b.toString(16), b);
+    console.log('toString(8) for oct', b.toString(8), b);
+    console.log('toString(2) for bin', b.toString(2), b);
+
+    console.log('Math constants');
+    console.log('E, root of the natural logarithm', Math.E);
+    console.log('PI, ratio of a circle circum to diameter', Math.PI);
+    console.log('LN2, natural logarithm of 2', Math.LN2);
+    console.log('SQRT2, squart root of 2', Math.SQRT2);
 };
