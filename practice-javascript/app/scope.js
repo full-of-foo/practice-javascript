a7ed9a71e11934c5a8a3e33cbbfe31c90d19fdc2
Anthony <lightweightdevelopment@gmail.com>
2017-04-02 22:41:30 +0100

    Added a little more scope

--- practice-javascript/app/scope.js
+++ practice-javascript/app/scope.js
@@ -52,6 +52,38 @@ export default () => {
             - we remove all vars/lets/consts and 'execute' the code.
             - LHS: 'foo' (target), RHS: 'bar' (source)
             - when I have an LHS: scope manager, does foo exist? Is it in our current scope? Can I have its ref?
-            - TODO: finish
+            - the fn bar gets executed
+            - LHS: 'foo', RHS: 'baz'
+            - scope manager, does foo exist? Is it in our current scope? Can I have its ref?
+            - the fn baz gets executed
+            - LHS: 'foo' (named param)
+            - scope manager, does foo exist? Is it in our current scope? Can I have its ref?
+            - LHS: 'foo' , RHS: 'bam'
+            - scope manager, does foo exist? Is it in our current scope? Can I have its ref?
+            - LHS: 'bam' , RHS: 'yay'
+            - scope manager, does bam exist? Is it in our current scope? Nope
+            - scope manager, does bam exist? Is it in our next (global) scope? Nope, so LET ME CREATE IT FOR YOU.
+              (Because we're not in strict mode and we could not find an LHS reference.
+               This is not the case for undeclared RHS references.
+               So note, undeclared !== undefined.
+               Undefined is more like uninitialised.)
     `);
 };
+
+console.log(`
+    var foo = 'bar';
+    function bar() {
+        var foo = 'baz';
+
+        function baz(foo) {
+            foo = 'bam';
+            bam = 'yay';
+        }
+        baz();
+    }
+    bar();
+    foo;
+    bam;
+    baz();
+    // TODO - what happens here in terms of scope and execution?
+`);
