030416732fbda120441cfa3727e4f5be32dda8a7
Anthony <lightweightdevelopment@gmail.com>
2017-04-06 19:42:43 +0100

    Finished Math

--- practice-javascript/app/math.js
+++ practice-javascript/app/math.js
@@ -1,8 +1,7 @@
 export default () => {
     console.log('Built in Math obj (for simple numeric stuff only): ', Math);
 
-    console.log('Number.prototype stuff');
-
+    console.log('Number.prototype stuff:');
     const a = 1.95;
     console.log('toFixed for fixing decimal (no arg or 0): ', a.toFixed(), a);
     console.log('toFixed (1): ', a.toFixed(1), a);
@@ -26,9 +25,31 @@ export default () => {
     console.log('toString(8) for oct', b.toString(8), b);
     console.log('toString(2) for bin', b.toString(2), b);
 
-    console.log('Math constants');
+    console.log('Math constants:');
     console.log('E, root of the natural logarithm', Math.E);
     console.log('PI, ratio of a circle circum to diameter', Math.PI);
     console.log('LN2, natural logarithm of 2', Math.LN2);
     console.log('SQRT2, squart root of 2', Math.SQRT2);
+
+    console.log('Exponentiation convenience functions:');
+    console.log('Math.pow for x^y: ', Math.pow(2, 3));
+    console.log('Math.sqrt for the square root of x: ', Math.sqrt(9));
+    console.log('Math.cbrt for the cube root of x: ', Math.cbrt(9));
+    console.log('Math.exp for the e^x: ', Math.exp(9));
+
+    console.log('Other convenience functions:');
+    console.log('Math.abs for the absolute (postive) val of x : ', Math.abs(-2), Math.abs(2));
+    console.log('Math.sign for the trinary bool of x: ', Math.sign(-2), Math.sign(2), Math.sign(0));
+    console.log('Math.trunc for the truncated int x : ',Math.trunc(-2.123), Math.trunc(2.123), Math.trunc(2));
+    console.log('Math.round for nearest int of x (.5 will round down): ',Math.trunc(-2.5), Math.trunc(2.5), Math.trunc(2.4));
+    console.log('Math.min/max for min/max value of args: ', Math.min(1, 0.5, -0.5, 2), Math.max(1, 0.5, -0.5, 2));
+
+    console.log('Trig functions:');
+    console.log('Math.sin for the sine x radians: ', Math.sin(Math.PI/2));
+    console.log('Math.cos for the cosine x radians: ', Math.cos(Math.PI));
+    console.log('Math.tan for the tangent x radians: ', Math.tan(Math.PI/4));
+
+    console.log('Pseudorandom numbers:');
+    console.log('Math.random for num >= 0 && num < 1: ', Math.random(), Math.random(), Math.random());
+    console.log('\'x + (y - x) * Math.random()\' for num between x and y: ', ((x, y) => x + (y - x) * Math.random())(1, 100));
 };
